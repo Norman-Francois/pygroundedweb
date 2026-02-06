fc3a9bb5ad32f06ba9d1f161ae242cf17e7bb1a1
Norman François <normanfrancois2004@gmail.com>
2026-02-07 00:44:05 +0100

    refactor: amélioration de du packaging du module

--- src/pygroundedweb/models/tools/sfm/__init__.py
+++ src/pygroundedweb/models/tools/sfm/__init__.py
@@ -1,7 +1,8 @@
 from typing import Union
 
 from .micmac import MicMac
+from .enums import DistortionModel, ZoomFinal, TapiocaMode
 
 SFM = Union[MicMac]
 
-__all__ = ["MicMac", "SFM"]
+__all__ = ["MicMac", "SFM", "DistortionModel", "ZoomFinal", "TapiocaMode"]
